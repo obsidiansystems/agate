6776f6a8a9500193d715ba477ecc174bf6d3bbd0
Cale Gibbard <cgibbard@gmail.com>
2025-09-22 01:08:03 -0400

    Initial commit, basically blank project setup.

---
+++ shell.nix
@@ -0,0 +1,16 @@
+# Enter a shell for this project using reflex-platform (which provides ghc8107)
+# or nixpkgs (which provides ghc943)
+{ compiler ? "ghc961" # or "ghc810"
+}:
+let pkgs = (import ./dep/reflex-platform { }).nixpkgs;
+in
+  pkgs.mkShell {
+    name = "shell-${compiler}";
+    buildInputs = [
+      pkgs.cabal-install
+      pkgs.ghcid
+    ];
+    inputsFrom = [
+      (import ./release.nix {}).${builtins.currentSystem}.${compiler}.env
+    ];
+  }
