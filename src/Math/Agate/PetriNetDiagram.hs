e8acff14ddf05016b42e9b908687cb64910abb5f
George Thomas <georgefsthomas@gmail.com>
2025-10-02 10:54:43 +0100

    Remove unnecessary `idPrefix` SVG option
    Only important when using multiple SVGs in one web page.

--- src/Math/Agate/PetriNetDiagram.hs
+++ src/Math/Agate/PetriNetDiagram.hs
@@ -1,14 +1,11 @@
 module Math.Agate.PetriNetDiagram where
 
 import Data.ByteString.Lazy qualified as BS
-import Data.Char
 import Data.List
-import Data.Text qualified as T
 import Diagrams.Backend.SVG
 import Diagrams.Prelude
 import Graphics.Svg (renderBS)
 import Math.Agate.Examples.ODE (runSolverSIR)
-import System.FilePath
 
 sirDiagram :: Double -> [Colour Double] -> [[Double]] -> Diagram B
 sirDiagram overallWidth colours sirData =
@@ -27,6 +24,6 @@ test :: IO ()
 test =
     BS.writeFile "/tmp/out.svg"
         . renderBS
-        . renderDia SVG (SVGOptions (mkSizeSpec (V2 (Just 1000) Nothing)) Nothing (T.filter isAlpha . T.pack $ takeBaseName "/tmp/out.svg") [] True)
+        . renderDia SVG (SVGOptions (mkSizeSpec (V2 (Just 1000) Nothing)) Nothing mempty [] True)
         $ sirDiagram 3 [red, blue, green]
         $ map (\(s, i, r) -> [i, s, r]) runSolverSIR
