0179fbb951d0ce71693bc242e9b900e84343f07c
George Thomas <georgefsthomas@gmail.com>
2025-10-07 15:32:28 +0100

    Fix GHCID script to work with multiple components

--- test/Main.hs
+++ test/Main.hs
@@ -1,11 +1,16 @@
-module Main (main) where
+module Main (main, mainAcceptAll) where
 
 import Test.Tasty
 import Tests.PetriNet (petriTests)
 import Tests.PetriNet.Chart (petriChartTest)
+import Test.Tasty.Golden.Manage (AcceptTests(AcceptTests))
 
 main :: IO ()
 main = defaultMain tests
+
+-- This is useful for regenerating outputs with GHCID.
+mainAcceptAll :: IO ()
+mainAcceptAll = defaultMain $ localOption (AcceptTests True) tests
 
 tests :: TestTree
 tests =
