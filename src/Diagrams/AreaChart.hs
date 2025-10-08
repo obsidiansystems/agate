458297dc85c6c7fce4b1c56147d208f99b3b31f8
George Thomas <georgefsthomas@gmail.com>
2025-10-08 14:36:24 +0100

    Avoid partial list functions in chart code
    The old definition of `adjacentPairs` is actually perfectly safe anyway, due to laziness, but still it's easy to rewrite it to avoid using `tail`.
    
    As for `head bottoms`, we could accept only non-empty lists and thread through a proof that `bottoms` remains non-empty. But this would require going through some doubly-non-empty type for expressing that `scanl` increases the length of a list by one, and `adjacentPairs` reduces by one again...

--- src/Diagrams/AreaChart.hs
+++ src/Diagrams/AreaChart.hs
@@ -2,6 +2,7 @@ module Diagrams.AreaChart (areaChart, Variable (..)) where
 
 import Control.Applicative
 import Data.List
+import Data.Maybe
 import Diagrams.Backend.SVG
 import Diagrams.Prelude
 
@@ -13,14 +14,14 @@ data Variable = Variable
 
 areaChartInner :: Double -> [Variable] -> Diagram B
 areaChartInner overallWidth sirData =
-    mconcat
-        $ zipWith
+    mconcat $
+        zipWith
             ( \(bottoms, tops) Variable{colour} ->
                 fromVertices
                     (zipWith (curry p2) [0, w ..] bottoms <> reverse (zipWith (curry p2) [0, w ..] tops))
                     & closeLine
                     & strokeLoop
-                    & translate (V2 0 (head bottoms))
+                    & translate (maybe 0 (V2 0) $ listToMaybe bottoms)
                     & fc colour
                     & lcA transparent
             )
@@ -30,7 +31,9 @@ areaChartInner overallWidth sirData =
   where
     w = overallWidth / maximum (map (genericLength . \Variable{values} -> values) sirData)
     adjacentPairs :: [a] -> [(a, a)]
-    adjacentPairs xs = zip xs $ tail xs
+    adjacentPairs = \case
+        [] -> []
+        x : xs -> zip (x : xs) xs
     mapColumns :: ([a] -> [b]) -> [[a]] -> [[b]]
     mapColumns f = transpose . zipWithN f
     zipWithN :: (Traversable t) => (t a -> b) -> t [a] -> [b]
