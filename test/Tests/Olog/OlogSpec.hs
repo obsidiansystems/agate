81824f190d48ce4d1ad5f87f445bc0087d34ce97
Felix Dilke <felix.dilke@obsidian.systems>
2025-10-10 15:51:43 +0100

    adding more tests, removed do-nothing ones, finding better structure

--- test/Tests/Olog/OlogSpec.hs
+++ test/Tests/Olog/OlogSpec.hs
@@ -7,30 +7,29 @@ import Test.Tasty.HUnit
 
 import Tests.PetriNet (exampleSIRODE)
 import Math.Agate.Olog.Olog(Arc(..), Identity(..), Olog(..), makeOlog, MakeOlogError(..))
+import Data.Either (isRight)
 
 type MaybeOlog = Either (MakeOlogError Int) (Olog Int)
 
 ologTests :: TestTree
 ologTests =
     testGroup
-        "Ologs basic properties"
-        [ testGroup
-            "basic identities"
-            [ testCase "Transitions Correct" $
-                assertBool "2 Transitions present" $
-                    (2 :: Int) == ((1 + 1) :: Int)
-            ],
-            testGroup
-            "further basic identities"
-            [ testCase "Transitions Correct" $
-                assertBool "2 Transitions present" $
-                    (4 :: Int) == ((2 + 2) :: Int)
-            ],
+        "Ologs basic properties" [ 
             testGroup "olog sanity checks" [
                 testCase "arcs must have a source of a known dot" $
                     let badOlog :: MaybeOlog
                         badOlog =
                             makeOlog [1] [("source", 0, 1)] []
-                    in badOlog @?= Left (UnknownSource "source" 0)
+                    in badOlog @?= Left (UnknownSource "source" 0),
+                testCase "arcs must have a target of a known dot" $
+                    let badOlog :: MaybeOlog
+                        badOlog =
+                            makeOlog [0] [("source", 0, 1)] []
+                    in badOlog @?= Left (UnknownTarget "source" 1),
+                testCase "arc is then ok" $
+                    let goodOlog :: MaybeOlog
+                        goodOlog =
+                            makeOlog [0, 1] [("y", 1, 0), ("x", 0, 1)] []
+                    in isRight goodOlog @?= True
             ]
         ]
