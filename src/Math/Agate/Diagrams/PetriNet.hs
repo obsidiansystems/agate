a321bac74cd5b8504d9ee6e7c69595f2177d4171
Patrick Aldis <patricktaldis@gmail.com>
2025-10-10 14:51:18 +0100

    visual improvements to petri diagram

--- src/Math/Agate/Diagrams/PetriNet.hs
+++ src/Math/Agate/Diagrams/PetriNet.hs
@@ -31,6 +31,9 @@ class VertexShow a where
   vShow :: a -> String
 
 instance {-# OVERLAPPING #-} VertexShow String where vShow s = s
+instance {-# OVERLAPPING #-} VertexShow Double where
+  vShow = showFixed @E3 True . realToFrac
+
 instance {-# OVERLAPPABLE #-} (Show a) => VertexShow a where vShow = show
 
 layoutPetri :: (Ord p, Ord t, VertexShow p, VertexShow t) => PetriNetImpl p t -> GraphvizCommand -> IO (Gr (AttributeNode (Vertex p t)) (AttributeNode Int))
@@ -68,19 +71,32 @@ drawPetriDynamic vertexColour marking = drawPetri' vShow \p -> elementToDiagram 
                 ]
 
 drawPetri' ::
-    ( Ord p, Ord t, VertexShow t) =>
-    (p -> String) ->
-    (p -> Diagram B) ->
-    Gr (AttributeNode (Vertex p t)) (AttributeNode Int) ->
-    Diagram B
-drawPetri' showPlace renderPlace = drawGraph
-        ( \v -> place $ case v of
-            Place p -> fontSizeL 10 (fc black (text (showPlace p))) <> renderPlace p <> fc white (circle 10)
-            Transition _ t -> fontSizeL 5 (fc white (text (vShow t))) <> fc black (square 10)
-        )
-        (\_ p1 _ p2 w p -> arrowBetween' (opts p w) p1 p2)
+  (Ord p, Ord t, VertexShow t) =>
+  (p -> String) ->
+  (p -> Diagram B) ->
+  Gr (AttributeNode (Vertex p t)) (AttributeNode Int) ->
+  Diagram B
+drawPetri' showPlace renderPlace =
+  drawGraph
+    ( \v -> place $ case v of
+        Place p ->
+          mconcat
+            [ text (showPlace p) & font fname & fontSizeL 10 & fc black,
+              renderPlace p,
+              circle 10 & fc white
+            ]
+        Transition _ t ->
+          mconcat
+            [ text (vShow t) & font fname & fontSizeL 5 & fc white,
+              square 15 & fc black
+            ]
+    )
+    (\_ p1 _ p2 w p -> arrowBetween' (opts p w) p1 p2)
   where
+    fname = "Latin Modern Math"
     opts p w =
-        with & gaps .~ local 10
-            & arrowShaft .~ (unLoc . fromMaybe (error "arrow has no path") . listToMaybe $ pathTrails p)
-            & headLength .~ local (5 * fromIntegral w)
+      with
+        & gaps .~ local 15
+        & arrowShaft .~ (unLoc . fromMaybe (error "arrow has no path") . listToMaybe $ pathTrails p)
+        & headLength .~ local (5 * fromIntegral w)
+        & arrowHead .~ arrowheadThorn (150 @@ deg)
