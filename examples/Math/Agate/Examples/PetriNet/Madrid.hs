6401c2b13e53b7219cd5dbe29ae0a4b34da24729
George Thomas <georgefsthomas@gmail.com>
2025-10-21 16:41:07 +0100

    Use enum for Madrid Petri places

--- examples/Math/Agate/Examples/PetriNet/Madrid.hs
+++ examples/Math/Agate/Examples/PetriNet/Madrid.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeOperators #-}
 
@@ -7,11 +6,21 @@ module Math.Agate.Examples.PetriNet.Madrid (madridNet) where
 import Data.List.NonEmpty qualified as NE
 import Math.Agate.PetriNet
 
-madridNet :: (Place net ~ String, Transition net ~ Double, PetriNet net) => net
+data MadridPlace
+    = C
+    | N
+    | E
+    | SE
+    | S
+    | W
+    | NW
+    deriving (Eq, Ord, Show)
+
+madridNet :: (Place net ~ MadridPlace, Transition net ~ Double, PetriNet net) => net
 madridNet =
     mconcat
         [ transition [s] 1 [t] <> transition [t] 1 [s]
-        | (s, t) <- (("C",) <$> outer) ++ zip outer (NE.tail (NE.fromList (cycle outer)))
+        | (s, t) <- ((C,) <$> outer) ++ zip outer (NE.tail (NE.fromList (cycle outer)))
         ]
   where
-    outer = ["N", "E", "SE", "S", "W", "NW"]
+    outer = [N, E, SE, S, W, NW]
