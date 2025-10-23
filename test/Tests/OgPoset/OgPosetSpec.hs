d9a519b1a5a0d3d0f03be652a59a14628858d8b2
Felix Dilke <felix.dilke@obsidian.systems>
2025-10-23 15:50:18 +0100

    tidy up verification some more

--- test/Tests/OgPoset/OgPosetSpec.hs
+++ test/Tests/OgPoset/OgPosetSpec.hs
@@ -57,81 +57,71 @@ ogPosetTests =
         checkExample11 maybePoset = do
           case maybePoset of
             Left err -> assertFailure $ "Failed to create OgPoset: " ++ show err
-            Right poset ->
-              let
-                verifyXFaces ::
-                  (OgFaceTable FancyInt -> Map FancyInt (Set FancyInt)) ->
-                  [(FancyInt, [FancyInt])] -> Assertion
-                verifyXFaces xfaces expectedValues =
-                  xfaces poset @?= expectedMap where
-                    expectedMap :: (Map FancyInt (Set FancyInt)) =
-                      Set.fromList <$> Map.fromList expectedValues
-               in
-                do
-                  grades poset
-                    @?= Map.fromList
-                      [ (0, Set.fromList [(0, 0), (0, 1), (0, 2), (0, 3)])
-                      , (1, Set.fromList [(1, 0), (1, 1), (1, 2), (1, 3)])
-                      , (2, Set.fromList [(2, 0)])
-                      ]
-                  for_ [0, 1, 2, 3] (\n -> grade poset (0, n) @?= Just 0)
-                  for_ [0, 1, 2, 3] (\n -> grade poset (1, n) @?= Just 1)
-                  grade poset (2, 0) @?= Just 2
-                  verifyXFaces infaces [
-                    ((0, 0), []), 
-                    ((0, 1), []), 
-                    ((0, 2), []),
-                    ((0, 3), []),
-                    ((1, 0), [(0, 0)]),
-                    ((1, 1), [(0, 1)]),
-                    ((1, 2), [(0, 2)]),
-                    ((1, 3), [(0, 0)]),
-                    ((2, 0), [(1, 0), (1, 1)])
-                    ]
-                  verifyXFaces outfaces [
-                    ((0, 0), []), 
-                    ((0, 1), []), 
-                    ((0, 2), []),
-                    ((0, 3), []),
-                    ((1, 0), [(0, 1)]),
-                    ((1, 1), [(0, 2)]),
-                    ((1, 2), [(0, 3)]),
-                    ((1, 3), [(0, 2)]),
-                    ((2, 0), [(1, 3)])
-                    ]
-                  verifyXFaces incofaces [
-                    ((0, 0), [(1, 0), (1, 3)]),
-                    ((0, 1), [(1, 1)]),
-                    ((0, 2), [(1, 2)]),
-                    ((0, 3), []),
-                    ((1, 0), [(2, 0)]),
-                    ((1, 1), [(2, 0)]),
-                    ((1, 2), []),
-                    ((1, 3), []),
-                    ((2, 0), [])
-                    ]
-                  verifyXFaces outcofaces [
-                    ((0, 0), []),
-                    ((0, 1), [(1, 0)]),
-                    ((0, 2), [(1, 1), (1, 3)]),
-                    ((0, 3), [(1, 2)]),
-                    ((1, 0), []),
-                    ((1, 1), []),
-                    ((1, 2), []),
-                    ((1, 3), [(2, 0)]),
-                    ((2, 0), [])
-                    ]
-                  predecessors poset (0, 0) @?= Set.fromList [(0, 0)]
-                  predecessors poset (0, 1) @?= Set.fromList [(0, 1)]
-                  predecessors poset (0, 2) @?= Set.fromList [(0, 2)]
-                  predecessors poset (0, 3) @?= Set.fromList [(0, 3)]
-                  predecessors poset (1, 0) @?= Set.fromList [(0, 0), (0, 1), (1, 0)]
-                  predecessors poset (1, 1) @?= Set.fromList [(0, 1), (0, 2), (1, 1)]
-                  predecessors poset (1, 2) @?= Set.fromList [(0, 2), (0, 3), (1, 2)]
-                  predecessors poset (1, 3) @?= Set.fromList [(0, 0), (0, 2), (1, 3)]
-                  predecessors poset (2, 0) @?= Set.fromList [
-                    (0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 3), (2, 0)
-                    ]
+            Right poset -> do
+              grades poset
+                @?= Map.fromList
+                  [ (0, Set.fromList [(0, 0), (0, 1), (0, 2), (0, 3)])
+                  , (1, Set.fromList [(1, 0), (1, 1), (1, 2), (1, 3)])
+                  , (2, Set.fromList [(2, 0)])
+                  ]
+              for_ [0, 1, 2, 3] (\n -> grade poset (0, n) @?= Just 0)
+              for_ [0, 1, 2, 3] (\n -> grade poset (1, n) @?= Just 1)
+              grade poset (2, 0) @?= Just 2
+              verifyXFaces (infaces poset) [
+                ((0, 0), []), 
+                ((0, 1), []), 
+                ((0, 2), []),
+                ((0, 3), []),
+                ((1, 0), [(0, 0)]),
+                ((1, 1), [(0, 1)]),
+                ((1, 2), [(0, 2)]),
+                ((1, 3), [(0, 0)]),
+                ((2, 0), [(1, 0), (1, 1)])
+                ]
+              verifyXFaces (outfaces poset) [
+                ((0, 0), []), 
+                ((0, 1), []), 
+                ((0, 2), []),
+                ((0, 3), []),
+                ((1, 0), [(0, 1)]),
+                ((1, 1), [(0, 2)]),
+                ((1, 2), [(0, 3)]),
+                ((1, 3), [(0, 2)]),
+                ((2, 0), [(1, 3)])
+                ]
+              verifyXFaces (incofaces poset) [
+                ((0, 0), [(1, 0), (1, 3)]),
+                ((0, 1), [(1, 1)]),
+                ((0, 2), [(1, 2)]),
+                ((0, 3), []),
+                ((1, 0), [(2, 0)]),
+                ((1, 1), [(2, 0)]),
+                ((1, 2), []),
+                ((1, 3), []),
+                ((2, 0), [])
+                ]
+              verifyXFaces (outcofaces poset) [
+                ((0, 0), []),
+                ((0, 1), [(1, 0)]),
+                ((0, 2), [(1, 1), (1, 3)]),
+                ((0, 3), [(1, 2)]),
+                ((1, 0), []),
+                ((1, 1), []),
+                ((1, 2), []),
+                ((1, 3), [(2, 0)]),
+                ((2, 0), [])
+                ]
+              predecessors poset (0, 0) @?= Set.fromList [(0, 0)]
+              predecessors poset (0, 1) @?= Set.fromList [(0, 1)]
+              predecessors poset (0, 2) @?= Set.fromList [(0, 2)]
+              predecessors poset (0, 3) @?= Set.fromList [(0, 3)]
+              predecessors poset (1, 0) @?= Set.fromList [(0, 0), (0, 1), (1, 0)]
+              predecessors poset (1, 1) @?= Set.fromList [(0, 1), (0, 2), (1, 1)]
+              predecessors poset (1, 2) @?= Set.fromList [(0, 2), (0, 3), (1, 2)]
+              predecessors poset (1, 3) @?= Set.fromList [(0, 0), (0, 2), (1, 3)]
+              predecessors poset (2, 0) @?= Set.fromList [
+                (0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 3), (2, 0)
+                ]
        in
         testGroup
           "Constructing OgPoset's"
@@ -160,3 +150,11 @@ ogPosetTests =
                   ]
           ]
     ]
+
+verifyXFaces ::
+  Map FancyInt (Set FancyInt) ->
+  [(FancyInt, [FancyInt])] -> Assertion
+verifyXFaces xfaces expectedValues =
+  xfaces @?= expectedMap where
+    expectedMap :: (Map FancyInt (Set FancyInt)) =
+      Set.fromList <$> Map.fromList expectedValues
