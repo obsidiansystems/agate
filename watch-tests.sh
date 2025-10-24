76ca56dfca3c5c0cd5a6923d119bac38c56e9799
George Thomas <georgefsthomas@gmail.com>
2025-10-24 14:11:24 +0100

    Generalise GHCID script to take arguments
    We no longer always accept golden test changes. Old behaviour can be achieved by passing `--accept`.

--- watch-tests.sh
+++ watch-tests.sh
@@ -1,1 +1,1 @@
-ghcid "--command=cabal repl --enable-multi-repl tests lib:agate" -s ':set args --accept --color=always' -T Main.main -W
+ghcid "--command=cabal repl --enable-multi-repl tests lib:agate" -s ":set args --color=always $*" -T Main.main -W
