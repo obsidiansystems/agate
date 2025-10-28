688c9cd6bda7e901956265143349e3c8248cea29
George Thomas <georgefsthomas@gmail.com>
2025-10-28 14:09:58 +0000

    Specify exposed yellow as HSL to match other colour definitions

--- examples/Math/Agate/Examples/PetriNet/Colours.hs
+++ examples/Math/Agate/Examples/PetriNet/Colours.hs
@@ -8,13 +8,13 @@ module Math.Agate.Examples.PetriNet.Colours (
 
 import Data.Colour.RGBSpace (uncurryRGB)
 import Data.Colour.RGBSpace.HSL (hsl)
-import Diagrams.Prelude (Colour, sRGB, yellow)
+import Diagrams.Prelude (Colour, sRGB)
 
 susceptible :: Colour Double
 susceptible = uncurryRGB sRGB $ hsl 240 0.7 0.4
 
 exposed :: Colour Double
-exposed = yellow
+exposed = uncurryRGB sRGB $ hsl 60 1.0 0.5
 
 infected :: Colour Double
 infected = uncurryRGB sRGB $ hsl 0 0.7 0.55
