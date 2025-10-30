4b545ca3000a7e95e6b92dddcdd2854223395d8f
George Thomas <georgefsthomas@gmail.com>
2025-10-30 11:55:35 +0000

    Add SVG test for non-Petri exponential ODE

--- test/Tests/ODE/Solver.hs
+++ test/Tests/ODE/Solver.hs
@@ -1,10 +1,16 @@
 module Tests.ODE.Solver where
 
+import Data.Colour.Names
+import Data.List.Extra
+import Data.Maybe
+import Diagrams.AreaChart
 import Math.Agate.Examples.ODE.Exponential
 import Math.Agate.Examples.ODE.Malthusian
 import Math.Agate.Examples.ODE.SIR
 import Test.Tasty
+import Test.Tasty.Golden
 import Test.Tasty.HUnit
+import TestUtils
 
 odeSolverTests :: TestTree
 odeSolverTests =
@@ -13,6 +19,21 @@ odeSolverTests =
         [ testCase "Exponential ODE" $ do
             assertBool "Expected solution" (not (null runSolverExponential))
             assertBool "Expected positivity" (all (> 0) runSolverExponential)
+        , goldenVsString "Exponential ODE Chart" "test/outputs/ode/exponential/chart.svg"
+            . pure
+            . diagToSVGBS
+            $ areaChart
+                False
+                3
+                [ Variable
+                    { name = "x"
+                    , colour = green
+                    , values =
+                        map (fromMaybe (error "empty chunk in solver data") . listToMaybe)
+                            . chunksOf 100
+                            $ runSolverExponential
+                    }
+                ]
         , let result = take 100 runSolverSIR
            in testCase "ODE Solver SIR Model" $ do
                 assertBool "Expected solution" (not (null result))
