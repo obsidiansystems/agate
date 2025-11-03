994c994e22a795f7c048a5ec401e3331c6b6b506
Patrick Aldis <patrick.aldis@virginmedia.com>
2025-11-03 16:24:47 +0000

    add docstrings for epidemiological models

--- examples/Math/Agate/Examples/ODE/SIRD.hs
+++ examples/Math/Agate/Examples/ODE/SIRD.hs
@@ -4,6 +4,7 @@ import Data.Map (Map)
 import Math.Agate.Examples.PetriNet.SIRD
 import Math.Agate.ODE.Polynomial.Solver
 
+-- | SIRD model take from [this](https://arxiv.org/pdf/2206.03269) paper
 runSolverSIRD :: [Map SIRDPlace Double]
 runSolverSIRD = solvePetri sird \case
     S -> 0.95
