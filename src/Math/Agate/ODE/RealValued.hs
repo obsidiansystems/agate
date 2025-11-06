a38df55374d8da6f0031c04c2821e040b846f6d0
Patrick Aldis <patricktaldis@gmail.com>
2025-11-06 15:55:22 +0000

    reorder k v variables

--- src/Math/Agate/ODE/RealValued.hs
+++ src/Math/Agate/ODE/RealValued.hs
@@ -4,11 +4,11 @@ import Data.Map (Map)
 import Data.Map qualified as Map
 import Math.Agate.ODE
 
-type Assignment k v = v -> k
+type Assignment v k = v -> k
 
-newtype RealFunction k v = RealFunction (Assignment k v -> k)
+newtype RealFunction v k = RealFunction (Assignment v k -> k)
 
-instance (Num k) => Num (RealFunction k v) where
+instance (Num k) => Num (RealFunction v k) where
     (+) = promoteBinary (+)
     (-) = promoteBinary (-)
     (*) = promoteBinary (*)
@@ -16,29 +16,29 @@ instance (Num k) => Num (RealFunction k v) where
     abs = promoteUnary abs
     fromInteger n = RealFunction (const $ fromInteger n)
 
-instance (Fractional k) => Fractional (RealFunction k v) where
-    fromRational k = RealFunction (const $ fromRational k)
+instance (Fractional k) => Fractional (RealFunction v k) where
+    fromRational v = RealFunction (const $ fromRational v)
     (/) = promoteBinary (/)
 
-promoteBinary :: (k -> k -> k) -> RealFunction k v -> RealFunction k v -> RealFunction k v
+promoteBinary :: (k -> k -> k) -> RealFunction v k -> RealFunction v k -> RealFunction v k
 promoteBinary f (RealFunction f1) (RealFunction f2) = RealFunction (\a -> f1 a `f` f2 a)
 
-promoteUnary :: (k -> k) -> RealFunction k v -> RealFunction k v
+promoteUnary :: (k -> k) -> RealFunction v k -> RealFunction v k
 promoteUnary op (RealFunction f1) = RealFunction (op . f1)
 
-newtype RealValuedODE k v = RealValuedODE (Map v (RealFunction k v))
+newtype RealValuedODE v k = RealValuedODE (Map v (RealFunction v k))
 
-instance (Ord v, Show v, Eq k, Num k) => ODESystem (RealValuedODE k v) where
-    type Exp (RealValuedODE k v) = RealFunction k v
-    type Var (RealValuedODE k v) = v
-    type Field (RealValuedODE k v) = k
+instance (Ord v, Show v, Eq k, Num k) => ODESystem (RealValuedODE v k) where
+    type Exp (RealValuedODE v k) = RealFunction v k
+    type Var (RealValuedODE v k) = v
+    type Field (RealValuedODE v k) = k
     v += e = RealValuedODE (Map.singleton v e)
     var v = RealFunction (\assignment -> assignment v)
     eval a (RealFunction f) = f a
     derivative (RealValuedODE eqns) v = eqns Map.! v
 
-instance (Ord v, Show v, Eq k, Num k) => Semigroup (RealValuedODE k v) where
+instance (Ord v, Show v, Eq k, Num k) => Semigroup (RealValuedODE v k) where
     RealValuedODE m1 <> RealValuedODE m2 = RealValuedODE (Map.unionWith (+) m1 m2)
 
-instance (Ord v, Show v, Eq k, Num k) => Monoid (RealValuedODE k v) where
+instance (Ord v, Show v, Eq k, Num k) => Monoid (RealValuedODE v k) where
     mempty = RealValuedODE Map.empty
