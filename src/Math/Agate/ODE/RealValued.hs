1d4d873cac04e8e89ea5a277f1ada7b364523693
Patrick Aldis <patricktaldis@gmail.com>
2025-11-06 16:16:16 +0000

    add <%>

--- src/Math/Agate/ODE/RealValued.hs
+++ src/Math/Agate/ODE/RealValued.hs
@@ -1,4 +1,4 @@
-module Math.Agate.ODE.RealValued (RealValuedODE (..), RealFunction (..), promoteUnary) where
+module Math.Agate.ODE.RealValued (RealValuedODE (..), RealFunction (..), (<%>)) where
 
 import Data.Map (Map)
 import Data.Map qualified as Map
@@ -16,6 +16,7 @@ instance (Num k) => Num (RealFunction v k) where
     abs = promoteUnary abs
     fromInteger n = RealFunction (const $ fromInteger n)
 
+
 instance (Fractional k) => Fractional (RealFunction v k) where
     fromRational v = RealFunction (const $ fromRational v)
     (/) = promoteBinary (/)
@@ -25,6 +26,9 @@ promoteBinary f (RealFunction f1) (RealFunction f2) = RealFunction (\a -> f1 a `
 
 promoteUnary :: (k -> k) -> RealFunction v k -> RealFunction v k
 promoteUnary op (RealFunction f1) = RealFunction (op . f1)
+
+(<%>) :: (k -> k) -> RealFunction v k -> RealFunction v k
+(<%>) = promoteUnary
 
 newtype RealValuedODE v k = RealValuedODE (Map v (RealFunction v k))
 
