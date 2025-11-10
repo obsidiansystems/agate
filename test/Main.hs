b4a7c9316941822c075c2d7c3ff1a7cd1d5553b8
George Thomas <georgefsthomas@gmail.com>
2025-11-10 14:01:02 +0000

    Undo some accidental reversions
    This seemingly happened due to merge conflicts while working on Ologs and Ogposets.

--- test/Main.hs
+++ test/Main.hs
@@ -2,23 +2,19 @@ module Main (main) where
 
 import Test.Tasty
 import Tests.ODE.Solver (odeSolverTests)
-import Tests.PetriNet (petriTests)
-
-import Tests.Olog.OlogSpec (ologTests)
-import Tests.Olog.Olog2Spec (olog2Tests)
 import Tests.OgPoset.OgPosetSpec (ogPosetTests)
--- import Tests.PetriNet.Chart (petriChartTest)
+import Tests.Olog.Olog2Spec (olog2Tests)
+import Tests.Olog.OlogSpec (ologTests)
+import Tests.PetriNet (petriTests)
 
 main :: IO ()
 main =
     defaultMain $
         testGroup
             "Tests"
-            [
-                odeSolverTests,
-                -- petriChartTest,
-                petriTests,
-                ologTests,
-                olog2Tests,
-                ogPosetTests
+            [ odeSolverTests
+            , petriTests
+            , ologTests
+            , olog2Tests
+            , ogPosetTests
             ]
